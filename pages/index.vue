ba9fc331f82683cb7060c03cbc4dfdecc4c9f0e7
Achun1130 <zx255235@gmail.com>
2024-05-30 22:59:39 +0800

    fix: github push test

--- pages/index.vue
+++ pages/index.vue
@@ -1,9 +1,9 @@
 <script lang="ts" setup></script>
 
 <template>
-  <el-button @click="ElMessage('hello')">button</el-button>
+  <!-- <el-button @click="ElMessage('hello')">button</el-button>
   <ElButton :icon="ElIconEditPen" type="success">button</ElButton>
-  <LazyElButton type="warning">lazy button</LazyElButton>
+  <LazyElButton type="warning">lazy button</LazyElButton> -->
   <h1 class="text-display-l">{{ $t('hello') }} {{ $t('world') }}</h1>
 
   <el-button type="primary" @click="navigateTo({ path: '/demo' })">前往 Demo 總覽</el-button>
