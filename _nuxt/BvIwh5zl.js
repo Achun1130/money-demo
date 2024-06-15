32da0557018315cd8e25ecdac054c86e45cca578
Achun1130 <50685396+Achun1130@users.noreply.github.com>
2024-06-15 17:29:51 +0000

    Deploying to gh-pages from @ Achun1130/money-demo@3932892e0e8ef6bd72295a5e7372d97fa92190c6 🚀

---
+++ _nuxt/BvIwh5zl.js
@@ -0,0 +1,1 @@
+import{d as e,t,v as s}from"./BDaFmbO_.js";const r=e({__name:"test",setup(n){return(o,a)=>(t(),s("p",null,"Hello this is test page."))}});export{r as default};
---
+++ _nuxt/DQP02Rqm.js
@@ -0,0 +1,1 @@
+import{_ as r,U as t}from"./BDaFmbO_.js";const o={};function s(e,n){return t(e.$slots,"default")}const _=r(o,[["render",s]]);export{_ as default};
